5c946df5284cf6df16b7ccba273039a316bbbebf
Jacques <24863947@sun.ac.za>
2023-11-09 14:39:24 +0200

    [code] some commenting done

--- src/lib/logic.js
+++ src/lib/logic.js
@@ -1,16 +1,23 @@
 export default function logic(currentElement, data, root) {
+    // create an element and return it
     if (data.type == "create-element") {
         const elementType = data.element;
         const index = data.index;
 
+        // create a text node & return
         if (elementType == "text") {
             currentElement.append(data.content);
-
             return currentElement.lastChild;
+
+        // create an element node
         } else {
             // create element, add content and index
             const element = document.createElement(elementType);
+
+            // add given content inside element
             element.append(data.content);
+
+            // set element's index attr
             element.setAttribute("data-forge-index", index);
 
             if (currentElement.nodeType === Node.ELEMENT_NODE) {
@@ -23,11 +30,14 @@ export default function logic(currentElement, data, root) {
 
             return element;
         }
+
+    // return element that has given forge-index (to select it)
     } else if (data.type == "select-element") {
         return getElementByIndex(root, data.index);
     }
 }
 
+// get element, given root element, that has given data-forge-index
 function getElementByIndex(root, index) {
     return root.querySelector(`[data-forge-index='${index}']`);
 }
